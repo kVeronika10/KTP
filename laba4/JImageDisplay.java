5e430295536da6bea848e95263603979117985b9
kVeronika10 <47713360+kVeronika10@users.noreply.github.com>
2020-06-06 02:48:20 +0300

    Update JImageDisplay.java

--- laba4/JImageDisplay.java
+++ laba4/JImageDisplay.java
@@ -7,18 +7,18 @@ import static java.awt.image.BufferedImage.TYPE_INT_RGB;
 public class JImageDisplay extends JComponent {
     private BufferedImage image;
 
-    /** Конструктор класса **/
+  
     public JImageDisplay(int x, int y)
     {
-        image = new BufferedImage(x,y,TYPE_INT_RGB); //ширина высота и тип изображения
+        image = new BufferedImage(x,y,TYPE_INT_RGB); 
         setPreferredSize(new Dimension(x, y));
     }
-    /** Метод для отрисовки фракталов **/
+   
     public void paintComponent (Graphics g)
     {
         g.drawImage (image, 0, 0, image.getWidth(), image.getHeight(), null);
     }
-    /** Метод для установки всех пикселей изображения в черный цвет */
+   
     public void clearImage ()
     {
         for (int x = 0; x < image.getWidth(); x++)
@@ -29,7 +29,7 @@ public class JImageDisplay extends JComponent {
             }
         }
     }
-    /** Метод для установки всех пикселей изображения в определенный цвет */
+   
     public void drawPixel (int x, int y, int rgbColor)
     {
         image.setRGB(x, y, rgbColor);
